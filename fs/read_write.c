ea830a2b2f1c4a485cbfe2990e3092a1ad4a992f
Pieter Smith <pieter@boesman.nl>
2014-10-24 23:49:20 +0200

    fs: moved kernel_write to fs/read_write
    kernel_write shares infrastructure with the read_write translation unit but not
    with the splice translation unit. Grouping kernel_write with the read_write
    translation unit is more logical. It also paves the way to compiling out the
    splice group of syscalls for embedded systems that do not need them.
    
    Signed-off-by: Pieter Smith <pieter@boesman.nl>
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -1203,3 +1203,19 @@ COMPAT_SYSCALL_DEFINE5(pwritev, compat_ulong_t, fd,
 }
 #endif
 
+ssize_t kernel_write(struct file *file, const char *buf, size_t count,
+			    loff_t pos)
+{
+	mm_segment_t old_fs;
+	ssize_t res;
+
+	old_fs = get_fs();
+	set_fs(get_ds());
+	/* The cast to a user pointer is valid due to the set_fs() */
+	res = vfs_write(file, (__force const char __user *)buf, count, &pos);
+	set_fs(old_fs);
+
+	return res;
+}
+EXPORT_SYMBOL(kernel_write);
+
